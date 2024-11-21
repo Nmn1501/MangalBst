8efe5be16522f84db3df58e5d3f7f29657c1ae5c
Naman1501 <147094593+Nmn1501@users.noreply.github.com>
2024-11-21 21:11:05 +0530

    Initial commit

---
+++ src/components/ItemNav.jsx
@@ -0,0 +1,25 @@
+const ItemNav=()=>{
+    return<>
+    <div className="itemnav">
+    <div class="hstack gap-3 Navitem_bar" >
+  <div class="p-2">Offers</div>
+  <div class="p-2">Todays Deal</div>
+  <div class="p-2">Essential</div>
+  <div class="p-2">Pooja Item</div>
+  <div class="p-2">Grocery</div>
+  <div class="p-2">Dry Fruits</div>
+  <div class="p-2">Cleaning</div>
+  <div class="p-2">Soaps&Detergint</div>
+  <div class="p-2">Stationary</div>
+  <div class="p-2">Cold Drinks</div>
+  <div class="p-2">B1G1</div>
+ 
+</div>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    </div>
+    </>
+}
+
+export default ItemNav;
